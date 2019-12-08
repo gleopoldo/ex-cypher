14f1eeba42656d220d655a5c7f4eaa90cf979a05
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2019-12-08 13:51:27 -0300

    Add a stringify helper

---
+++ lib/ex_cypher/statement.ex
@@ -0,0 +1,25 @@
+defmodule ExCypher.Statement do
+  def stringify(element)
+       when is_atom(element),
+       do: Atom.to_string(element)
+
+  def stringify([]), do: ""
+
+  def stringify(props) when props == %{}, do: ""
+
+  def stringify(list)
+       when is_list(list),
+       do: ":#{Enum.join(list, ",")}"
+
+  def stringify(props = %{}) do
+    args =
+      props
+      |> Enum.into([])
+      |> Enum.map(fn {name, value} -> ~s["#{name}":"#{value}"] end)
+      |> Enum.join(",")
+
+    " {#{args}}"
+  end
+
+  def stringify(str), do: str
+end
