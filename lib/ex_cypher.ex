ee2fec3f5f03cc14212ca93a11f627ea99ecd85c
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2019-12-26 12:24:53 -0300

    Move CREATE statement to new parsing model

--- lib/ex_cypher.ex
+++ lib/ex_cypher.ex
@@ -125,8 +125,8 @@ defmodule ExCypher do
   alias ExCypher.Query
   alias ExCypher.Statement
 
-  @root_commands [:match, :create]
-  @supported_statements [:merge, :return, :where, :pipe_with, :order, :limit]
+  @root_commands [:match]
+  @supported_statements [:create, :merge, :return, :where, :pipe_with, :order, :limit]
 
   @helpers [:node, :--, :->, :<-, :rel]
 
