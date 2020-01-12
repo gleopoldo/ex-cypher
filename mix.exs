ad6e71a4b5b3df32eff9f4410fe374da412e5318
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2020-01-12 15:37:45 -0300

    Add code coverage to CI

--- mix.exs
+++ mix.exs
@@ -9,6 +9,12 @@ defmodule ExCypher.MixProject do
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test
+      ],
       deps: deps(),
       package: package()
     ]
@@ -25,6 +31,7 @@ defmodule ExCypher.MixProject do
   defp deps do
     [
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
