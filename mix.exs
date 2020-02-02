65dedfc1c988b9ce8c0f97a5659e6185d71c6a16
Gabriel Ferreira <gabrielleopferreira@gmail.com>
2020-02-01 21:42:14 -0300

    Bump excypher version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExCypher.MixProject do
     [
       app: :ex_cypher,
       description: "A DSL to interact with cypher query language",
-      version: "0.3.0-rc.1",
+      version: "0.3.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
